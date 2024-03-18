f31933c4f3a04e1f4f7df5e8ca6f2a97644dcdd1
ltphat2204 <ltphat2204@gmail.com>
2024-03-18 22:50:09 +0700

    Change API workflow and model

--- app/backend/server/common/jwt_token.go
+++ app/backend/server/common/jwt_token.go
@@ -3,17 +3,12 @@ package common
 import (
 	"github.com/golang-jwt/jwt/v5" //sign json web token
 	"os"                           // load .env
-	"time"                         // get now
 )
 
 func GenerateToken(data map[string]interface{}) (string, error) {
-	// Calculate expire time
-	expireTime := time.Now().Add(24 * time.Hour).Unix()
-
 	// Create token
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"username": data["username"],
-		"exp":      expireTime, //Expired in 24 hours
 	})
 
 	// Sign with secret key
--- app/backend/server/user/controllers.go
+++ app/backend/server/user/controllers.go
@@ -7,8 +7,8 @@ import (
 	"os"
 	"time"
 
-	"github.com/golang-jwt/jwt/v5"
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/miniminh/AIELTS/tree/main/app/backend/server/common"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -45,17 +45,8 @@ func AuthMiddleware(c *gin.Context) {
 
 	username, exists := claims["username"].(string)
 	if !exists {
-		log.Fatal("[AUTH-LOG] Token can not claim 2: " + token.Raw)
+		log.Fatal("[AUTH-LOG] Token can not claim: " + token.Raw)
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Token can not claim!"})
-		c.Abort()
-		return
-	}
-
-	expire, _ := claims.GetExpirationTime()
-
-	// Token expired
-	if expire.Time.Unix() < time.Now().Unix() {
-		c.JSON(http.StatusUnauthorized, gin.H{"message": "Expired token!"})
 		c.Abort()
 		return
 	}
@@ -86,8 +77,8 @@ func Verify(c *gin.Context) {
 	status, message, user := usernameExists(username.(string))
 	if status == http.StatusOK {
 		c.JSON(status, gin.H{"message": "Authentication successful", "user": UserResult{
-			Username: user.Username,
-			ID: user.ID,
+			Username:  user.Username,
+			ID:        user.ID,
 			CreatedAt: user.CreatedAt,
 		}})
 	} else {
